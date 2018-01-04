36de747c364922499199c7c60e5e66e612c6605e
Kokolihapihvi <antsa.s@hotmail.com>
2018-01-05 01:22:59 +0200

    Added color interpolation to accuracy percentage

--- addons/current_song_wtv/script.js
+++ addons/current_song_wtv/script.js
@@ -4,6 +4,24 @@ var preview = false;
 //Enable animation (percentage scrolls to the next number instead of snapping)
 var animate_percentage = true;
 
+//Enable colour animation (percentage fades from one color to another across 0%->100%)
+var animate_percentage_color = true;
+
+//Color for 0%
+var color_0 = "#FF0000";
+
+//Color for 50%
+var color_50 = "#FFFF00";
+
+//Color for 100%
+var color_100 = "#00FF00";
+
+//The point at which the 50% color is placed in the gradient, number between 0 and 1
+var color_midpoint = 0.8;
+
+//Exponent for interpolation, higher number = steeper curve towards the end, 1 = linear interpolation
+var color_exponent = 2;
+
 //JQuerys document.onReady function
 //Gets called after the webpage is loaded
 $(function() {
@@ -15,6 +33,25 @@ $(function() {
 		$("h1.accuracy_percentage").text("100%");
 		$("img.album_cover").attr("src", "rs_pick.png");
 		showPopup();
+
+		if(animate_percentage) {
+			testAnim = function() {
+				$("h1.accuracy_percentage").prop("number", 0).finish().animateNumber({
+						number: 100,
+						numberStep: function(now, tween) {
+							$(tween.elem).text(now.toFixed(2)+"%");
+
+							if(animate_percentage_color) {
+								$(tween.elem).css("color", lerpColors(now/100));
+							}
+						}
+					}, 5000);
+			};
+
+			testAnim();
+
+			setInterval(testAnim, 6000);
+		}
 	}
 	else {
 		//Set a timer to refresh our data every 1000 milliseconds
@@ -64,7 +101,11 @@ function refresh() {
 				$("h1.accuracy_percentage").prop("number", prev_accuracy).finish().animateNumber({
 					number: accuracy,
 					numberStep: function(now, tween) {
-						$(tween.elem).text(now.toFixed(2)+"%")
+						$(tween.elem).text(now.toFixed(2)+"%");
+
+						if(animate_percentage_color) {
+							$(tween.elem).css("color", lerpColors(now/100));
+						}
 					}
 				}, 1000);
 
@@ -109,4 +150,41 @@ function showPopup() {
 	//Do a fadein over 1000 milliseconds and set the visible variable to true
 	$("div.popup").fadeIn(1000);
 	visible = true;
+}
+
+//Lerp between the 0%, 50% and 100% colors
+function lerpColors(p) {
+	//Transform to logarithmic scale
+	p = Math.pow(p, color_exponent)
+
+	if(p <= color_midpoint) {
+		return lerpColor(color_0, color_50, p / color_midpoint);
+	} else {
+		return lerpColor(color_50, color_100, (p - color_midpoint) / (1 - color_midpoint));
+	}
+}
+
+/** https://gist.github.com/rosszurowski/67f04465c424a9bc0dae
+ * A linear interpolator for hexadecimal colors
+ * @param {String} a
+ * @param {String} b
+ * @param {Number} amount
+ * @example
+ * // returns #7F7F7F
+ * lerpColor('#000000', '#ffffff', 0.5)
+ * @returns {String}
+ */
+function lerpColor(a, b, amount) { 
+	//Clamp the amount to 0-1 range
+	amount = Math.min(1, Math.max(0,amount));
+
+    var ah = parseInt(a.replace(/#/g, ''), 16),
+        ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
+        bh = parseInt(b.replace(/#/g, ''), 16),
+        br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
+        rr = ar + amount * (br - ar),
+        rg = ag + amount * (bg - ag),
+        rb = ab + amount * (bb - ab);
+
+    return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
 }
