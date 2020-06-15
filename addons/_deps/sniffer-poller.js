adbeea7184ff191bad6ca031c40a184a7680f2fa
PoizenJam <65501967+PoizenJam@users.noreply.github.com>
2020-06-15 11:03:54 -0400

    Update sniffer-poller.js
    Updated code to add phrase functionality and and commented code for readability

--- addons/_deps/sniffer-poller.js
+++ addons/_deps/sniffer-poller.js
@@ -4,13 +4,15 @@ const STATE_SONG_SELECTED = 2;
 const STATE_SONG_STARTING = 3;
 const STATE_SONG_PLAYING = 4;
 const STATE_SONG_ENDING = 5;
+var defaultPath = "Bass";
 
 class SnifferPoller {
+	//Create variables and containers for poller data.
 	constructor(options = {}) {
 		var defaultOptions = {
 			ip: "127.0.0.1",
 			port: "9938",
-			interval: 900,
+			interval: 100,
 
 			onData: (data) => {},
 			onSongChanged: (songData) => {console.log("onSongChanged",songData)},
@@ -38,9 +40,12 @@ class SnifferPoller {
 		}
 	}
 
+	//Trigger for onData
 	onData(f) {
 		this.callbacks.onData.push(f);
 	}
+	
+	//Event triggers for onData
 	_doOnData(data) {
 		this.options.onData(data);
 
@@ -49,9 +54,12 @@ class SnifferPoller {
 		}
 	}
 
+	//Trigger for song change
 	onSongChanged(f) {
 		this.callbacks.onSongChanged.push(f);
 	}
+	
+	//Event triggers for song change
 	_doOnSongChanged(song) {
 		this.options.onSongChanged(song);
 
@@ -60,9 +68,12 @@ class SnifferPoller {
 		}
 	}
 
+	//Trigger for song start
 	onSongStarted(f) {
 		this.callbacks.onSongStarted.push(f);
 	}
+	
+	//Event triggers for song start
 	_doOnSongStarted(song) {
 		this.options.onSongStarted(song);
 
@@ -71,9 +82,12 @@ class SnifferPoller {
 		}
 	}
 
+	//Event triggers for song end
 	onSongEnded(f) {
 		this.callbacks.onSongEnded.push(f);
 	}
+	
+	//Event triggers for song end
 	_doOnSongEnded(song) {
 		this.options.onSongEnded(song);
 
@@ -82,9 +96,12 @@ class SnifferPoller {
 		}
 	}
 
+	//Set up event triggers
 	onStateChanged(f) {
 		this.callbacks.onStateChanged.push(f);
 	}
+	
+	//Event triggers when game state changes
 	_doOnStateChanged(oldState, newState) {
 		this.options.onStateChanged(oldState, newState);
 
@@ -93,7 +110,7 @@ class SnifferPoller {
 		}
 	}
 
-
+	//Set event triggers when data
 	gotData(data) {
 		//If we have no previous data, fire all events
 		if(!this._prevdata) {
@@ -137,22 +154,27 @@ class SnifferPoller {
 		this._doOnData(data);
 	}
 
+	//Get current data
 	getCurrentReadout() {
 		return this._prevdata.memoryReadout;
 	}
 
+	//Get current song
 	getCurrentSong() {
 		return this._prevdata.songDetails;
 	}
 
+	//Get current game state
 	getCurrentState() {
 		return this._prevdata.currentState;
 	}
 
+	//Get song timer
 	getSongTimer() {
 		return this._prevdata.memoryReadout.songTimer;
 	}
 
+	//Get current accuract
 	getCurrentAccuracy(decimals = 2) {
 		//Get accuracy
 		var accuracy = this._prevdata.memoryReadout.noteData.Accuracy;
@@ -161,6 +183,7 @@ class SnifferPoller {
 		return parseFloat(accuracy.toFixed(decimals));
 	}
 
+	//Get current arrangement
 	getCurrentArrangement() {
 		if(!this._prevdata) {
 			return null;
@@ -170,23 +193,41 @@ class SnifferPoller {
 			return null;
 		}
 
-		if(!this._prevdata.memoryReadout.arrangementID) {
-			return null;
-		}
-
+		//Look through arrangements for matching ID
 		for (var i = this._prevdata.songDetails.arrangements.length - 1; i >= 0; i--) {
 			var arrangement = this._prevdata.songDetails.arrangements[i];
 
-			if(arrangement.arrangementID == this._prevdata.memoryReadout.arrangementID) {
+			//Check that ID is correctly formatted-32 characters long-to avoid errors
+			if(arrangement.arrangementID.length == 32 && arrangement.arrangementID == this._prevdata.memoryReadout.arrangementID) {
+				return arrangement;
+			}
+		}
+		
+		//If no matching ID, look for arragnement that matches previous path
+		for (var i = this._prevdata.songDetails.arrangements.length - 1; i >= 0; i--) {
+			var arrangement = this._prevdata.songDetails.arrangements[i];	
+			
+			if(this.prevPath != null && arrangement.name == this.prevPath  && arrangement.type == this.prevPath  && arrangement.isBonusArrangement == false && arrangement.isAlternateArrangement == false) {
+				return arrangement;
+			}
+		}
+		
+		//If no previous path, resort to default path listed at top
+		for (var i = this._prevdata.songDetails.arrangements.length - 1; i >= 0; i--) {
+			var arrangement = this._prevdata.songDetails.arrangements[i];	
+			
+			if(arrangement.name == defaultPath && arrangement.type == defaultPath && arrangement.isBonusArrangement == false && arrangement.isAlternateArrangement == false) {
 				return arrangement;
 			}
 		}
 	}
 
+	//Get section at current time
 	getCurrentSection() {
 		return this.getSectionAt(this.getSongTimer());
 	}
 
+	//Get section at specific time
 	getSectionAt(time) {
 		var arrangement = this.getCurrentArrangement();
 
@@ -204,6 +245,47 @@ class SnifferPoller {
 		}
 
 		return arrangement.sections[0];
+	}
+	
+	//Get phrase at current time
+	getCurrentPhrase() {
+		return this.getPhraseAt(this.getSongTimer());
+	}
+	
+	//Get phrase at specific time
+	getPhraseAt(time) {
+		var arrangement = this.getCurrentArrangement();
+
+		if(!arrangement) {
+			return null;
+		}
+
+		for (var i = arrangement.phraseIterations.length-1; i >= 0; i--) {
+			var phrase = arrangement.phraseIterations[i];
+			phrase.index = i;
+
+			if(time > phrase.startTime) {
+				return phrase;
+			}
+		}
+
+		return arrangement.phraseIterations[0];
+	}
+	
+	//get Maximum Difficulty from an arrangement
+	getMaxDif(){
+		var arrangement = this.getCurrentArrangement();
+		if(!arrangement) {
+			return 0;
+		}
+		var maxDif = 0;			
+		for (var i = 0; i < arrangement.phraseIterations.length; i++) { 
+			var phrase = arrangement.phraseIterations[i];
+			if(phrase.maxDifficulty > maxDif){
+				maxDif = phrase.maxDifficulty;					
+			}
+		}	
+		return maxDif;
 	}
 
 	poll() {
