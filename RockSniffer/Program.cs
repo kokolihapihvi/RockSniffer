70ffaa4a02d7fa0589d8df8b0114095c26f1b045
tjwrona <tjwrona1992@gmail.com>
2023-05-20 15:15:06 -0400

    Updated version number

--- RockSniffer/Program.cs
+++ RockSniffer/Program.cs
@@ -23,7 +23,7 @@ namespace RockSniffer
 {
     class Program
     {
-        internal const string version = "0.4.1";
+        internal const string version = "0.4.1-buddy";
 
         internal static ICache cache;
         internal static Config config;
